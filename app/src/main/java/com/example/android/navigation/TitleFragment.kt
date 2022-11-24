7fdbb53c2d3c8096fa3e589ab674b6d2236a0bce
Argam <mehrabyanargam111@gmail.com>
2022-11-24 19:01:27 +0400

    navigation integration,

---
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,35 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+    lateinit var binding: FragmentTitleBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+        // Inflate the layout for this fragment
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+//            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+    }
+}
+
+
+
+
